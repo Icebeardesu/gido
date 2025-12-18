89cdfc54dd62999d21b35b17a2ed82286c39f357
Trungkien06-tn <nghiemkien2006@gmail.com>
2025-12-18 14:09:34 +0700

    Chỉnh sửa link

--- views/productcatalog.php
+++ views/productcatalog.php
@@ -426,7 +426,7 @@
                                             </a>
                                             <div class="overlay">
                                                 <div class="cart-area">
-                                                    <a class="add-cart-btn" href="cart-page.html"><i class="bi bi-bag-check"></i>
+                                                    <a class="add-cart-btn" href="index.php?page=addtocart&id=<?= $item['id_san_pham'] ?>"><i class="bi bi-bag-check"></i>
                                                         Add To Cart</a>
                                                 </div>
                                             </div>
